592ed69eef754ab90db0b0adc9c44410548964a4
zph <zph@localhost.localdomain>
2018-12-10 13:09:39 +0800

    zph

--- NetCoreWeb/Superhui.Web/Areas/Docs/Controllers/File.cs
+++ NetCoreWeb/Superhui.Web/Areas/Docs/Controllers/File.cs
@@ -13,6 +13,7 @@ using Superhui.Web.Areas.Docs.Models.ViewModels;
 using Superhui.Web.Models.ViewModels;
 using Superhui.Web.Infrastructure.Filters;
 using Superhui.Tls;
+using Newtonsoft.Json.Linq;
 
 namespace Superhui.Web.Areas.Docs.Controllers
 {
@@ -32,10 +33,29 @@ namespace Superhui.Web.Areas.Docs.Controllers
             {
                 path += ".md";
             }
-            var nr = NetworkRequest.CreateHttp($"http://localhost:5000/api/file/content/{path}");
+            // var nr = NetworkRequest.CreateHttp($"http://localhost:5000/api/file/content/{path}");
+            var nr = NetworkRequest.CreateHttp($"http://localhost:5000/api/file/fileInfo/{path}");
             string fContent = await nr.GetAsync<string>();
             return View((object)fContent);
         }
+        // public async Task<ActionResult> Index(string path = "")
+        // {
+        //     path = path.Trim('/');
+        //     if(!path.EndsWith(".md"))
+        //     {
+        //         path += ".md";
+        //     }
+        //     // var cataloguePath = path.Substring(0, path.LastIndexOf('/') + 1);
+        //     var nr = NetworkRequest.CreateHttp($"http://localhost:5000/api/file/content/{path}");
+        //     string fContent = await nr.GetAsync<string>();
+
+        //     var catalogueInfoRequest = NetworkRequest.CreateHttp($"http://localhost:5000/api/file/fileInfo/note/linux");
+        //     string catalogueInfo = await catalogueInfoRequest.GetAsync<string>();
+        //     JObject o = JObject.Parse(catalogueInfo);
+        //     JArray fileInfoArray = (JArray)o.SelectToken("children");
+        //     ViewBag.Catalogue = fileInfoArray;
+        //     return View((object)fContent);
+        // }
         public async Task<ActionResult> List1(string path = "")
         {
             path = path.Trim('/');
